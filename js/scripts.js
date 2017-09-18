cb2881ff2dda2885ffe2f06421e985184ab363bc
Kimberly Bordon and Kyle Ebersole <kyle@>
2017-09-18 15:53:44 -0700

    Add filter function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,10 @@ $(document).ready(function(){
   $("#wordPlay").submit(function(event) {
     var sentence = $("input#sentence").val();
     var sentenceArray = sentence.split(" ");
-    var newSentenceArray = [];
 
-    sentenceArray.forEach(function(word) {
-      if (word.length >= 3) {
-        newSentenceArray.push(word);
-      }
-    });
+    var newSentenceArray = sentenceArray.filter(function(word) {
+      return word.length >= 3;
+      });
 
     newSentenceArray.reverse();
       var fixString = newSentenceArray.join(" ");
